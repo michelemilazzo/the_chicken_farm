cf98e65c8f7caaccaec9f4637fdd06614b3fde70
OneKey MMOS <ops@onekeyco.com>
2026-06-14 10:33:39 +0200

    feat: app logo icon (tcf_logo_circle.svg)

--- the_chicken_farm/hooks.py
+++ the_chicken_farm/hooks.py
@@ -4,6 +4,7 @@ app_publisher = "BMAS S.r.l."
 app_description = "Restaurant operations, HACCP, franchise and academy system"
 app_email = "info@thechickenfarm.it"
 app_license = "mit"
+app_logo_url = "/assets/the_chicken_farm/images/tcf_logo_circle.svg"
 
 required_apps = ["frappe", "erpnext", "webshop"]
 
